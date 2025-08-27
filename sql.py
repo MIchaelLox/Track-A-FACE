19d9a6af2d00c231fc1537cc4f68a299cd66abc3
MIchaelLox <ghalimiaratrading@gmail.com>
2025-08-27 15:04:34 -0400

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,1 +1,29 @@
+# ==========================
+# /sql.py
+# ==========================
 
+
+"""
+This module defines the database schema.
+Tables:
+- inputs (user-provided values)
+- cost_factors (weights, multipliers, coefficients)
+- outputs (calculated results)
+"""
+
+
+class DatabaseSchema:
+def __init__(self):
+# TODO: Set up connection details (if using SQLite, Postgres, etc.)
+pass
+
+
+def create_tables(self):
+# TODO: Define SQL CREATE TABLE statements for inputs, cost_factors, outputs
+# Execute against DB connection
+pass
+
+
+def seed_cost_factors(self):
+# TODO: Insert base multipliers and coefficients into cost_factors table
+pass
