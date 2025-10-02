ec5a880a4957aca2c4f0283240f69c2056e7def9
Diatoufall <diatoufall278@gmail.com>
2025-10-02 02:06:18 -0400

    validation temps réel formulaire saisie  - ValidationHelper: 5 règles de validation métier - Validation sur btnCalculate_Click - Messages d'erreur utilisateur clairs"

---
+++ TrackAFaceUI/TrackAFaceWinForms/TrackAFaceWinForms/Helpers/ValidationHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackAFaceWinForms.Helpers
+{
+    internal class ValidationHelper
+    {
+    }
+}
